8b091efb05bee92269ba1a441744fbed7a1a01f2
antonio32a <~@antonio32a.com>
2023-11-07 21:41:10 +0100

    Make every patch configurable

--- modinfo.lua
+++ modinfo.lua
@@ -14,5 +14,35 @@ client_only_mod = false
 all_clients_require_mod = false
 server_only_mod = true
 
-configuration_options = {}
-mod_dependencies = {}
+configuration_options = {
+    {
+        name = "PATCH_GLOBAL_POSITIONS",
+        label = "Patch Global Positions",
+        hover = "Should the mod patch the crash and spawn in prefab exploits from Global Positions?",
+        options = {
+            { description = "Yes", data = true },
+            { description = "No", data = false }
+        },
+        default = true
+    },
+    {
+        name = "PATCH_SHOW_ME",
+        label = "Patch Show Me",
+        hover = "Should the mod patch the crash exploit from Show Me?",
+        options = {
+            { description = "Yes", data = true },
+            { description = "No", data = false }
+        },
+        default = true
+    },
+    {
+        name = "WRAP_ALL_RPC_HANDLERS",
+        label = "Wrap All RPC Handlers",
+        hover = "Should the mod wrap all RPC handlers to prevent crashes from other mods?",
+        options = {
+            { description = "Yes", data = true },
+            { description = "No", data = false }
+        },
+        default = false
+    }
+}
--- modmain.lua
+++ modmain.lua
@@ -17,30 +17,36 @@ local SHOW_ME = "ShowMeSHint"
 local Patcher = require("patcher")
 
 AddSimPostInit(function()
-    Patcher.Patch(GLOBAL_POSITIONS, "Ping", function(_, pingtype, x, y, z)
-        if not checkstring(pingtype) or not checknumber(x) or not checknumber(y) or not checknumber(z) then
-            return "Invalid data types."
-        end
-
-        if pingtype ~= "delete" and pingtype ~= "clear" then
-            local prefab = "ping_" .. pingtype
-            if not PrefabExists(prefab) then
-                return "Provided ping prefab does not exist."
+    if GetModConfigData("PATCH_GLOBAL_POSITIONS") then
+        Patcher.Patch(GLOBAL_POSITIONS, "Ping", function(_, pingtype, x, y, z)
+            if not checkstring(pingtype) or not checknumber(x) or not checknumber(y) or not checknumber(z) then
+                return "Invalid data types."
             end
-        end
-    end)
 
-    Patcher.Patch(SHOW_ME, "Hint", function(_, guid, item)
-        if not checknumber(guid) or not checkentity(item) then
-            return "Invalid data types."
-        end
-    end)
+            if pingtype ~= "delete" and pingtype ~= "clear" then
+                local prefab = "ping_" .. pingtype
+                if not PrefabExists(prefab) then
+                    return "Provided ping prefab does not exist."
+                end
+            end
+        end)
+    end
 
-    Patcher.HookAllHandlers(function(namespace, rpc_name, player, oldfn, ...)
-        local status, result = pcall(oldfn, player, ...)
-        if not status then
-            local display_name = player.name .. " (" .. player.userid .. ")"
-            print("RPC Patcher: RPC " .. rpc_name .. " from namespace " .. namespace .. " from " .. display_name .. " errored. Error: " .. tostring(result))
-        end
-    end)
+    if GetModConfigData("PATCH_SHOW_ME") then
+        Patcher.Patch(SHOW_ME, "Hint", function(_, guid, item)
+            if not checknumber(guid) or not checkentity(item) then
+                return "Invalid data types."
+            end
+        end)
+    end
+
+    if GetModConfigData("WRAP_ALL_RPC_HANDLERS") then
+        Patcher.HookAllHandlers(function(namespace, rpc_name, player, oldfn, ...)
+            local status, result = pcall(oldfn, player, ...)
+            if not status then
+                local display_name = player.name .. " (" .. player.userid .. ")"
+                print("RPC Patcher: RPC " .. rpc_name .. " from namespace " .. namespace .. " from " .. display_name .. " errored. Error: " .. tostring(result))
+            end
+        end)
+    end
 end)
